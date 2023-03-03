1bb8ccb672e2e8df9c85382067bdcc9d006ee662
jose.aldana <jaldana@crackthecode.la>
2023-03-02 19:15:37 -0500

    d
    d

--- dataBase.js
+++ dataBase.js
@@ -71,30 +71,54 @@ $("#guardar").click( async function () {
 $("#listaBitacora").on("click", "#editar", async function () {
   var bitacoraId = $(this).closest('div').attr('id');
   var editado = doc(db, "BDBitacoras", bitacoraId);
+  var nuevoTitulo = "";
+  var nuevoContenido = "";
+
+  // Create the popup form
+  var popupForm = $("<div></div>");
+  popupForm.attr('id', 'myForm');
+  popupForm.addClass("modal fade");
+  popupForm.append('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Editar Bitácora</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><form><div class="form-group"><label for="input1">Título</label><input type="text" class="form-control" id="input1" value=""></div><div class="form-group"><label for="input2">Contenido</label><textarea class="form-control" id="input2" rows="3"></textarea></div></form></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button><button type="button" class="btn btn-primary" id="saveBtn">Guardar</button></div></div></div>');
+
+  // Append the popup form to the body
+  $("body").append(popupForm);
+
+  // Get the current data and fill in the inputs of the popup form
+  var bitacoraDB = await getDoc(doc(db, "BDBitacoras", bitacoraId));
+  nuevoTitulo = bitacoraDB.data().titulo;
+  nuevoContenido = bitacoraDB.data().contenido;
+  $('#input1').val(nuevoTitulo);
+  $('#input2').val(nuevoContenido);
+
+  // Show the popup form
   $('#myForm').modal('show');
 
+  // Save the new data when the "Guardar" button is clicked
   $('#saveBtn').click(async function() {
-    var nuevoTitulo = $('#input1').val();
-    var nuevoContenido = $('#input2').val();
-    
+    nuevoTitulo = $('#input1').val();
+    nuevoContenido = $('#input2').val();
+
     await updateDoc(editado, {
-      titulo : nuevoTitulo,
+      titulo: nuevoTitulo,
       contenido: nuevoContenido
     });
 
     // Update the corresponding HTML elements with the new data from Firestore
-    var bitacoraDB= await getDoc(doc(db,"BDBitacoras", bitacoraId));
+    var bitacoraDB= await getDoc(doc(db, "BDBitacoras", bitacoraId));
     $('#' + bitacoraId + ' h1').text(bitacoraDB.data().titulo);
     $('#' + bitacoraId + ' h4').text(bitacoraDB.data().contenido);
-    
-    $('#myForm').modal('hide');
+
+    // Destroy the popup form
+    $('#myForm').modal("hide");
+    $('#myForm').remove();
   });
 });
 
 $("#listaBitacora").on("click", "#eliminar", async function () {
-
+  var bitacoraId = $(this).closest('div').attr('id');
   await deleteDoc(doc(db, "BDBitacoras", bitacoraId));
-
+  $(this).closest("div").remove(); // remove the parent card element of the clicked button
   alert("eliminaste");
 });
+
 
